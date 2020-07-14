f9a84dcd2b79c7d022956b93134290c2ec4f14db
Arique Aguilar <arique1104@gmail.com>
2020-07-14 10:51:55 -0500

    Creates renters method for Building class

--- test/building_test.rb
+++ test/building_test.rb
@@ -35,14 +35,12 @@ class BuildingTest < Minitest::Test
   def test_it_can_add_renters
     # skip
     renter1 = Renter.new("Aurora")
-    # renter2 = Renter.new("Tim")
 
     unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
 
     unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
 
     unit1.add_renter(renter1)
-    # unit2.add_renter(renter2)
 
     building = Building.new
 
@@ -50,6 +48,10 @@ class BuildingTest < Minitest::Test
     building.add_unit(unit2)
 
     assert_equal ["Aurora"], building.renters
+
+    renter2 = Renter.new("Tim")
+    unit2.add_renter(renter2)
+    assert_equal ["Aurora", "Tim"], building.renters
   end
 
 end
