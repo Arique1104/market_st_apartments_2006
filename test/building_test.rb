41f433ac286df38a78cf0ee6917d118f7e14af0c
Arique Aguilar <arique1104@gmail.com>
2020-07-14 10:25:35 -0500

    Creates Building class test it exists

--- test/building_test.rb
+++ test/building_test.rb
@@ -7,9 +7,10 @@ require "./lib/building"
 
 class BuildingTest < Minitest::Test
 
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  def test_it_exists
+    building = Building.new
+    assert_instance_of Building, building
+  end
 
   #def test_it_has_attributes
   #end
