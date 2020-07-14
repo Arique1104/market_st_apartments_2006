11dc6b9ef37e244f1446a9c5173c0ba7334f256b
Arique Aguilar <arique1104@gmail.com>
2020-07-14 10:20:14 -0500

    Creates bedroom insta var for apt class

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,12 +1,14 @@
 class Apartment
  attr_reader  :number,
               :monthly_rent,
-              :bathrooms
+              :bathrooms,
+              :bedrooms
 
   def initialize(unit_specs)
     @number = unit_specs[:number]
     @monthly_rent = unit_specs[:monthly_rent]
     @bathrooms = unit_specs[:bathrooms]
+    @bedrooms = unit_specs[:bedrooms]
 
   end
 
