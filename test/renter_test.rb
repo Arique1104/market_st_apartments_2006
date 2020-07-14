c3e88e6d4f3f6bcd555c5debcfc03d88d3f2ebe2
Arique Aguilar <arique1104@gmail.com>
2020-07-14 10:05:44 -0500

    Test it exists complete

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -5,9 +5,10 @@ require "./lib/renter"
 
 class RenterTest < Minitest::Test
 
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  def test_it_exists
+    renter = Renter.new("Jessie")
+    assert_instance_of Renter, renter
+  end
 
   #def test_it_has_attributes
   #end
