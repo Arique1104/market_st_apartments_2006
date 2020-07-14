8c4ec0b7fb496c09c99d3954083fba2d16b4763e
Arique Aguilar <arique1104@gmail.com>
2020-07-14 10:19:28 -0500

    Creates @bathroom instance var in apt class

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,9 +1,12 @@
 class Apartment
  attr_reader  :number,
-              :monthly_rent
+              :monthly_rent,
+              :bathrooms
+
   def initialize(unit_specs)
     @number = unit_specs[:number]
     @monthly_rent = unit_specs[:monthly_rent]
+    @bathrooms = unit_specs[:bathrooms]
 
   end
 
